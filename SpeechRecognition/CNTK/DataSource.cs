cba5d061f0662ccea0c24f9b4378a91df068010f
Andriy <andriy.rom1294@gmail.com>
2017-04-08 21:51:40 +0300

    Implemented data structures for dense data and text files.

---
+++ SpeechRecognition/CNTK/DataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.CNTK {
+    public class DataSource : IDataSource {
+        private IEnumerator<ISample> Iterator;
+
+        private List<ISample> Samples;
+
+        public DataSource(string name, IList<ISample> samples) {
+            Name = name;
+            Samples = new List<ISample>(samples);
+            Iterator = Samples.GetEnumerator();
+        }
+
+        public string Name { get; private set; }
+
+        public ISample NextSample() {
+            return Iterator.MoveNext() ? Iterator.Current : null;
+        }
+
+        public void Reset() {
+            Iterator.Reset();
+        }
+    }
+}
---
+++ SpeechRecognition/CNTk/DenseSample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.CNTK {
+    public class DenseSample : ISample {
+        private double[] SampleElements;
+
+        public DenseSample(double[] data) {
+            SampleElements = new double[data.Length];
+            data.CopyTo(SampleElements, 0);
+        }
+
+        public string GetString() {
+            StringBuilder sampleElementsString = new StringBuilder(); 
+            foreach(double element in SampleElements) {
+                sampleElementsString.Append(string.Format("{1} ", element));
+            }
+            return sampleElementsString.ToString();
+        }
+
+        public byte[] GetBytes() {
+            throw new NotImplementedException();
+        }
+    }
+}
