e2eea44c30e63c99ea3dd075047031727032dcb5
Andriy <andriy.rom1294@gmail.com>
2017-04-09 00:28:15 +0300

    Implemented converter for double feature arrays into DataSource.

---
+++ SpeechRecognition/CNTK/DenseSampleDataConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.CNTK {
+    public class DenseSampleDataConverter {
+        private List<IDataSource> DataSources = new List<IDataSource>();
+
+        public void AddDataSource(string name, IList<double[]> samples) {
+            IList<ISample> dataSamples = samples.Select(sample => new DenseSample(sample) as ISample).ToList();
+            DataSource dataSource = new DataSource(name, dataSamples);
+        }
+
+        public DataContainer GetDataContainer() {
+            return new DataContainer(DataSources);
+        }
+
+    }
+}
