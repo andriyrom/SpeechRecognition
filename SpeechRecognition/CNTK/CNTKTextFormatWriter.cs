b65e0d328d70cf39777be22abb9c8b0f972d698f
Andriy <andriy.rom1294@gmail.com>
2017-04-08 18:49:07 +0300

    Implemented writer for CNTKTextFormat

---
+++ SpeechRecognition/CNTK/CNTKTextFormatWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.CNTK {
+    public class CNTKTextFormatWriter : IFeatureWriter {
+        private const string FeatureTemplate = "|{1} {2}";
+
+        public void WriteToFile(DataContainer data, string filePath) {
+            FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+            WriteToStream(data, fileStream);
+            fileStream.Close();
+        }
+
+        public void WriteToStream(DataContainer data, Stream stream) {
+            IList<IDataSource> dataSources = data.GetDataSources();
+            string[] dataSourceNames = data.GetDataSourceNames().ToArray();
+            StringBuilder textFileContent = new StringBuilder();
+            List<ISample> currentSamples = GetSamples(dataSources);
+            while (currentSamples.Any()) {
+                AddFeatureString(textFileContent, dataSourceNames, currentSamples);
+                currentSamples = GetSamples(dataSources);
+            }
+            ResetDataSources(dataSources);
+            WriteContentToFile(textFileContent.ToString(), stream);
+        }
+
+        private List<ISample> GetSamples(IList<IDataSource> dataSources) {
+            return dataSources.Select(dataSource => dataSource.NextSample()).ToList();
+        }
+
+        private void ResetDataSources(IList<IDataSource> dataSources) {
+            foreach(IDataSource dataSource in dataSources) {
+                dataSource.Reset();
+            }
+        }
+
+        private void AddFeatureString(StringBuilder textFileContent, string[] dataSourceNames, IList<ISample> currentSamples) {
+            for (int i = 0; i < dataSourceNames.Length; i++) {
+                string featureString = string.Format(FeatureTemplate, dataSourceNames[i], currentSamples[i].GetString());
+                textFileContent.Append(featureString);
+            }
+            textFileContent.AppendLine();
+        }
+
+        private void WriteContentToFile(string fileContent, Stream file) {
+            StreamWriter writter = new StreamWriter(file);
+            writter.Write(fileContent);
+        }
+    }
+}
