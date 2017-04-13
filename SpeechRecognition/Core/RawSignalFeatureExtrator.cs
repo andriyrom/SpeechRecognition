537a2c25538b98fd99bab37a6d4ba0cfb42604f1
Andriy <andriy.rom1294@gmail.com>
2017-04-14 00:18:39 +0300

    Added feature RawSignal features extractor. It trims silence around vowels and extracts sound signal as a feature vector.

---
+++ SpeechRecognition/Core/RawSignalFeatureExtrator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.Core {
+    public class RawSignalFeatureExtrator : IFeatureExtractor {
+        private ISpeechDetector SpeechDetector;
+
+        public RawSignalFeatureExtrator()
+            : this(new SimpleSpeechDetector()) { }
+
+        public RawSignalFeatureExtrator(ISpeechDetector speechDetector) {
+            SpeechDetector = speechDetector;
+        }
+
+        public FeaturesContainer GetFeatures(SignalContainer signal) {
+            SignalContainer speech = SpeechDetector.GetSpeech(signal);
+            var speechBounds = new SpeechBounds(0, speech.Signal.Length - 1);
+            var features = new FrameFeatures(speechBounds, speech.Signal);
+            return new FeaturesContainer(new List<FrameFeatures>() { features });
+        }
+    }
+}
