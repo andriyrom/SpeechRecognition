9db80f95319983841d86ef73505d4147f99ff56c
Andriy <andriy.rom1294@gmail.com>
2017-04-26 18:53:59 +0300

    Implemented feature extractor with FilterBank core and usage of repeat signal.

---
+++ SpeechRecognition/Core/FilterBankParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.Core {
+    public class FilterBankParameters {
+        public int StartFrequency { get;  private set; }
+ 
+        public int BandWidth { get;  private set; }
+ 
+        public int Count { get;  private set; }
+ 
+        public double SampleRate { get;  private set; }
+
+        public int Order { get; private set; }
+
+        public FilterBankParameters(int startFrequency, int bandWidth, int filterCount, double sampleRate, int filterOrder) {
+            StartFrequency = startFrequency;
+            BandWidth = bandWidth;
+            Count = filterCount;
+            SampleRate = sampleRate;
+            Order = filterOrder;
+        }
+    }
+}
---
+++ SpeechRecognition/Core/RepeatFilterBankFeatureExtractor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition.Core {
+    public class RepeatFilterBankFeatureExtractor : IFeatureExtractor {
+        private FilterBank FilterBankCore;
+        private int RepeatFrameTimes;
+        private int TakeFrameNumber;
+
+        public RepeatFilterBankFeatureExtractor(FilterBankParameters filterBankSettings, int repeatFrameTimes, int takeFrameNumber) {
+            FilterBankCore = new FilterBank(filterBankSettings.StartFrequency,
+                filterBankSettings.BandWidth,
+                filterBankSettings.Count,
+                filterBankSettings.SampleRate,
+                filterBankSettings.Order);
+            RepeatFrameTimes = repeatFrameTimes;
+            TakeFrameNumber = takeFrameNumber;
+        }
+
+        public FeaturesContainer GetFeatures(SignalContainer signal) {
+            IList<Frame> frames = signal.GetFrames();
+            List<FrameFeatures> features = new List<FrameFeatures>();
+            foreach (Frame frame in frames) {
+                SignalContainer extendedSignal = GetSignalForFiltering(frame.Signal);
+                IList<FilterProcessResult> extendedSignalChannels = FilterBankCore.ProcessSignal(extendedSignal);
+                FilterBankCore.Reset();
+                IList<FilterProcessResult> signalChannels = GetFilteredSignalsPart(extendedSignalChannels, frame.Bounds.Width);
+                double[] channelsNormalizedEnergy = GetChannelsEnergy(signalChannels);
+                features.Add(new FrameFeatures(frame.Bounds, channelsNormalizedEnergy));
+            }
+            return new FeaturesContainer(features);
+        }
+
+        private SignalContainer GetSignalForFiltering(double[] rawSignal) {
+            double[] extendedSignal = Enumerable.Repeat(rawSignal, RepeatFrameTimes)
+                .SelectMany(array => array).ToArray();
+            return new SignalContainer(extendedSignal);
+        }
+
+        private List<FilterProcessResult> GetFilteredSignalsPart(IList<FilterProcessResult> extendedResults, int originalSignalLength) {
+            return extendedResults.Select(channelSignal => GetFilteredSingalForOutput(channelSignal, originalSignalLength)).ToList();
+        }
+
+        private FilterProcessResult GetFilteredSingalForOutput(FilterProcessResult extendedResult, int originalSignalLength) {
+            double[] filteredSignal = extendedResult.FilteredSignal.Skip(TakeFrameNumber * originalSignalLength)
+                .Take(originalSignalLength).ToArray();
+            return new FilterProcessResult(extendedResult.Band, filteredSignal);
+        }
+
+        private double[] GetChannelsEnergy(IList<FilterProcessResult> channelsSignal) {
+            var channelsRawEnergy = channelsSignal.Select(channel => SignalHelper.GetEnergy(channel.FilteredSignal, 0, channel.FilteredSignal.Length));
+            double maxEnergy = channelsRawEnergy.Max(channel => Math.Abs(channel));
+            return channelsRawEnergy.Select(channelEnergy => channelEnergy / maxEnergy).ToArray();
+        }
+    }
+}
