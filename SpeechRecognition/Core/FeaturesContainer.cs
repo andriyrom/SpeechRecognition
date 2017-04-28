a3c9bfab3691d7ec4cee76c32da176f3f289805a
Andriy <andriy.rom1294@gmail.com>
2017-04-28 16:51:48 +0300

    Added feature: FeatureContainer has method to split current container in several smaller.

--- SpeechRecognition/Core/FeaturesContainer.cs
+++ SpeechRecognition/Core/FeaturesContainer.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SpeechRecognition.Core {
+    public enum SplitContainerMode {
+        DiscardFractionalPart = 0,
+        ExtendFractionalPart = 1
+    }
+
     public class FeaturesContainer {
         private List<FrameFeatures> SignalFeatures;
 
@@ -39,6 +44,38 @@ namespace SpeechRecognition.Core {
 
         public List<double[]> GetSeparately() {
             return SignalFeatures.Select(frameFeatures => frameFeatures.Features).ToList();
+        }
+
+        public List<FeaturesContainer> SplitIntoContainers(int frameSize, int frameShift, SplitContainerMode lastPartAction) {
+            List<FeaturesContainer> splittedContainers = new List<FeaturesContainer>();
+            int startIndex = 0;
+            int endIndex = frameSize - 1;
+            int lastFeatureFrameIndex = SignalFeatures.Count - 1;
+            while (endIndex < lastFeatureFrameIndex) {
+                splittedContainers.Add(GetCurrentContainer(startIndex, endIndex, frameSize));
+                startIndex += frameShift;
+                endIndex = startIndex + frameSize - 1;                
+            }
+            if (endIndex == lastFeatureFrameIndex) {
+                return splittedContainers;
+            }
+            if (lastPartAction == SplitContainerMode.ExtendFractionalPart) {
+                splittedContainers.Add(GetExtendedContainer(startIndex, endIndex, lastFeatureFrameIndex));
+            }
+            return splittedContainers;
+        }
+
+        private FeaturesContainer GetCurrentContainer(int startIndex, int endIndex, int frameSize) {
+            IEnumerable<FrameFeatures> currentContainerFeatures = SignalFeatures.Skip(startIndex).Take(frameSize);
+            return new FeaturesContainer(currentContainerFeatures);
+        }
+
+        private FeaturesContainer GetExtendedContainer(int startIndex, int endIndex, int lastFeatureFrameIndex) {
+            var partFeatures = SignalFeatures.Skip(startIndex);
+            FrameFeatures lastFrame = partFeatures.Last();
+            var extendedPart = Enumerable.Repeat(lastFrame, endIndex - lastFeatureFrameIndex);
+            List<FrameFeatures> extendedContainerFeatures = partFeatures.Concat(extendedPart).ToList();
+            return new FeaturesContainer(extendedContainerFeatures);
         }
     }
 }
